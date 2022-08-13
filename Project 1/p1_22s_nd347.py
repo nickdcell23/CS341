4773b1b2a90994bbd048e5454488742684e86314
nickdcell23 <81056558+nickdcell23@users.noreply.github.com>
2022-08-13 15:20:57 -0400

    Add files via upload
    Added CS341 semester projects

---
+++ Project 1/p1_22s_nd347.py
@@ -0,0 +1,247 @@
+# Nicholas Digioia-Celentano
+# nd347
+# CS341-452 Project 1
+
+import sys
+from queue import Queue
+
+# A stack seems like an easy way to make this program work
+# We can make functions for each state in the DFA and do stack.pop on each call
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+def main():
+
+    # Information requested by instructor
+    print("Project 1 for CS 341 \n Section number: 452 \n Semester: Spring 2022 \n Written by: Nicholas Digioia-Celentano, nd347 \n Instructor: Marvin Nakayama, marvin@njit.edu")
+
+    # Have user select if they want to enter a string, analyze it if yes
+    yesno = input("Would you like to enter a string for analyzing? (y/n): ")
+    while (yesno != 'y' and yesno != 'n'):
+        print("Invalid. Please write y or n")
+        yesno = input("Would you like to enter a string for analyzing? (y/n): ")
+
+    while yesno == 'y':
+        user_input = input("Please enter your string: ")
+        stack = Stack()
+        print(f"{user_input}")
+
+        # Push the string backward so first in first out can be achieve using stack
+        for i in user_input[::-1]:
+            stack.push(i)
+            result = True;
+
+        result = startState(stack)
+        if result:
+            print("String is valid!\n")
+        else:
+            print("String is invalid!\n")
+
+        # Ask user if they want to enter another string
+        yesno = input("Would you like to enter the string? (y/n): ")
+        while (yesno != 'y' and yesno != 'n'):
+            print("Invalid. Please write y or n")
+            yesno = input("Would you like to enter a string for analyzing? (y/n): ")
+
+        # EMPTY THE STACK!!!
+        emptyStack(stack)
+
+# And here's our starting state, q0
+def startState(stack):
+
+    print("Start state: q0")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return False
+    nextchar = stack.peek().lower()
+
+    if (nextchar.isalpha()):
+        return q1(stack)
+
+    return trapState(stack)
+
+# State q1
+def q1(stack):
+
+    print(f"{stack.pop()} --- State: q1")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return False
+    nextchar = stack.peek().lower()
+
+    if (nextchar.isalpha()):
+        return q1(stack)
+
+    elif (nextchar == '.'):
+       return q2(stack)
+
+    elif (nextchar == '@'):
+        return q3(stack)
+
+    return trapState(stack)
+
+# State q2
+def q2(stack):
+
+    print(f"{stack.pop()} --- State: q2")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return False
+    nextchar = stack.peek().lower()
+
+    if (nextchar.isalpha()):
+        return q1(stack)
+
+    return trapState(stack)
+
+
+# State q3
+def q3(stack):
+
+    print(f"{stack.pop()} --- State: q3")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return False
+    nextchar = stack.peek().lower()
+
+    if (nextchar.isalpha()):
+        return q4(stack)
+
+    return trapState(stack)
+
+# State q4
+def q4(stack):
+
+    print(f"{stack.pop()} --- State: q4")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return False
+    nextchar = stack.peek().lower()
+
+    if (nextchar.isalpha()):
+        return q4(stack)
+
+    elif (nextchar == '.'):
+        return q5(stack)
+
+    return trapState(stack)
+
+# State q5
+def q5(stack):
+
+    print(f"{stack.pop()} --- State: q5")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return False
+    nextchar = stack.peek().lower()
+
+    if (nextchar == 'o'):
+        return q6(stack)
+
+    elif (nextchar.isalpha()):
+        return q3(stack)
+
+    return trapState(stack)
+
+# State q6
+def q6(stack):
+
+    print(f"{stack.pop()} --- State: q6")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return False
+    nextchar = stack.peek().lower()
+
+    if (nextchar == 'r'):
+        return q7(stack)
+
+    elif (nextchar == '.'):
+        return q5(stack)
+
+    elif (nextchar.isalpha()):
+        return q4(stack)
+
+    return trapState(stack)
+
+# State q7
+def q7(stack):
+
+    print(f"{stack.pop()} --- State: q7")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return False
+    nextchar = stack.peek().lower()
+
+    if (nextchar == 'g'):
+        return q8(stack)
+
+    elif (nextchar == '.'):
+        return q5(stack)
+
+    if (nextchar.isalpha()):
+        return q4(stack)
+
+    return trapState(stack)
+
+# State q8
+def q8(stack):
+
+    print(f"{stack.pop()} --- State: q8")
+
+    # Empty check
+    if (stack.isEmpty()):
+        return True
+    nextchar = stack.peek().lower()
+
+    if (nextchar == '.'):
+        return q5(stack)
+
+    elif (nextchar.isalpha()):
+        return q4(stack)
+
+    return trapState(stack)
+
+# Function used to empty stack
+def emptyStack(stack):
+
+    while (not stack.isEmpty()):
+
+        stack.pop()
+
+# Our trap state, aka q9
+def trapState(stack):
+
+    print("Trap State!")
+
+    while not stack.isEmpty():
+        print(f"{stack.pop()} --- Trap State: q9")
+
+    return False
+
+if __name__ == "__main__":
+    main()
---
+++ Project 2/p2_22s_nd347.py
@@ -0,0 +1,158 @@
+# Nicholas Digioia-Celentano
+# nd347
+# CS341-452, Spring '22, Project 2, Professor Nakayama
+
+# import fileinput
+
+
+# using a stack to remember inputs
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+def pdacheck(string):
+    s = Stack()  # initialize the stack
+    sym = '+-*/'  # our set of valid symbols
+    state = 1  # initial state is  q1
+    accept_state = 7  # define our accepting state for easier access
+    print('\nInitial State = q{}'.format(state))  # specify what our initial state is
+
+    for i in string:
+
+        # first character MUST be &, check if it is then push onto stack. go state 2
+        if i == '&' and state == 1:
+            s.push('&')
+            state = 2
+            print('{}, Ɛ -> &: Current state = q{}'.format(i, state))  # this will make formatting way easier
+
+        # every ( must have a ). remember this ( by pushing it onto stack
+        elif i == '(' and state == 2:
+            s.push('(')
+            state = 2
+            print('{}, Ɛ -> (: Current state = q{}'.format(i, state))
+
+        # if a number at state 2, we move to state 3
+        elif i.isdigit() and state == 2:
+            state = 3
+            print('{}, Ɛ -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a dot at state 2, we move to state 4
+        elif i == '.' and state == 2:
+            state = 4
+            print('{}, Ɛ -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a number at state 3, we stay at state 3
+        elif i.isdigit() and state == 3:
+            state = 3
+            print('{}, Ɛ -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a . at state 3, we move to state 5
+        elif i == '.' and state == 3:
+            state = 5
+            print('{}, Ɛ -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a digit at state 4, we move to state 5
+        elif i.isdigit() and state == 4:
+            state = 5
+            print('{}, Ɛ -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a digit at state 5, we stay at state 5
+        elif i.isdigit() and state == 5:
+            state = 5
+            print('{}, Ɛ -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a & at state 5, check if the & is at the top of the stack (beginning of string)
+        elif i == '&' and state == 5:
+            if s.peek() == '&':
+                s.pop()
+                state = accept_state  # if it is, accept it. otherwise, crash
+            else:
+                break
+            print('{}, & -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a symbol at state 5, go back to state 2
+        elif i in sym and state == 5:
+            state = 2
+            print('{}, Ɛ -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a ) at state 5, check for a ( at top of stack, move to state 6. if not, break
+        elif i == ')' and state == 5:
+            if s.peek() == '(':
+                s.pop()
+                state = 6
+            else:
+                break
+            print('{}, ( -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a ) at state 6, check for a ( at top of stack, stay in state 6. if not, break
+        elif i == ')' and state == 6:
+            if s.peek() == '(':
+                s.pop()
+                state = 6
+            else:
+                break
+            print('{}, ( -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a symbol at state 6, go back to state 2
+        elif i in sym and state == 6:
+            state = 2
+            print('{}, Ɛ -> Ɛ: Current state = q{}'.format(i, state))
+
+        # if a & at state 6, check if & is at top of stack (beginning of string)
+        elif i == '&' and state == 6:
+            if s.peek() == '&':
+                s.pop()
+                state = accept_state  # if it is, accept it. otherwise, crash
+            else:
+                break
+            print('{}, & -> Ɛ: Current state = q{}'.format(i, state))
+
+        # any invalid symbols, letters, etc? "Crash"
+        else:
+            break
+
+    # acceptance state. if state is not 7, something went wrong. tell them it crashed
+    if state == 7:
+        print("String", string, "is accepted! \n")
+    else:
+        print("Program crashed at state {} due to invalid character {}".format(state, i))
+        print("String", string, "is denied! \n")
+
+
+# basic i/o for string analyzing
+def main():
+    while True:
+        yesno = input("Would you like to enter a string to analyze? (y/n) ")
+        if yesno.lower() == 'n':
+            print("Thank you, and goodbye!")
+            break
+        elif yesno.lower() == 'y':
+            string = input("Enter string to analyze: ")
+            pdacheck(string)
+        else:
+            print("Please enter a valid command! (y/n)")
+
+# note to Professor Nakayama
+# when writing console output to a .txt file, for some reason the character Ɛ is converted to ?
+# when reading my output, assume all ?'s with arrows on either side are the character Ɛ.
+# i apologize for the inconvenience
+
+
+if __name__ == "__main__":
+    main()
